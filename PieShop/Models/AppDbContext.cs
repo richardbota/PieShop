7916352882e0a7f6ed211550c6161a0e186573e5
admin <richard.bota@khk.ee>
2018-09-16 08:44:44 +0300

    Initialized database

--- PieShop/Models/AppDbContext.cs
+++ PieShop/Models/AppDbContext.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace PieShop.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
     }
 }
--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -14,7 +16,27 @@ namespace PieShop
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+
+                    //we could log this in a real-world situation
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
